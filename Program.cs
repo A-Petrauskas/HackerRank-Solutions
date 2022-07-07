af6f5ab1f7f0c39c6e6f7f78537c8e516c0cef84
Audrius Petrauskas <audrius.j.petrauskas@gmail.com>
2022-07-07 20:12:14 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HackerRank_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
